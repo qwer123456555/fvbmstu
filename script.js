3bc52bd0f4bc7f37b8f11595df6f75f13afd097f
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 19:15:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const startButton = document.getElementById('startButton');
-  const showDataButton = document.getElementById('showDataButton');
+document.addEventListener('DOMContentLoaded', () => {
   const fullNameInput = document.getElementById('fullName');
+  const startButton = document.getElementById('startButton');
+  const container = document.querySelector('.container');
   const scoreElement = document.getElementById('score');
   const visitsElement = document.getElementById('visits');
-  const pressNormative = document.getElementById('pressNormative');
-  const swimNormative = document.getElementById('swimNormative');
-  const dataContainer = document.querySelector('.data-container');
-  const normativesContainer = document.querySelector('.normatives-container');
+  const swimNormativeElement = document.getElementById('swimNormative');
+  const pressNormativeElement = document.getElementById('pressNormative');
+  const showDataButton = document.getElementById('showDataButton');
 
-  startButton.addEventListener('click', function() {
-    document.querySelector('.splash-container').style.display = 'none';
-    document.querySelector('.container').style.display = 'block';
+  startButton.addEventListener('click', () => {
+    const fullName = fullNameInput.value.toLowerCase();
+    const students = {
+      'иван иванов': {
+        score: 70,
+        visits: 25,
+        swimNormative: '✅',
+        pressNormative: '❌',
+      },
+      'петр петров': {
+        score: 40,
+        visits: 21,
+        swimNormative: '❌',
+        pressNormative: '✅',
+      },
+    };
+
+    if (fullName in students) {
+      const student = students[fullName];
+      scoreElement.textContent = student.score;
+      visitsElement.textContent = student.visits;
+      swimNormativeElement.textContent = student.swimNormative;
+      pressNormativeElement.textContent = student.pressNormative;
+    } else {
+      scoreElement.textContent = '';
+      visitsElement.textContent = '';
+      swimNormativeElement.textContent = '';
+      pressNormativeElement.textContent = '';
+      alert('Такого ученика нет');
+    }
   });
 
-  showDataButton.addEventListener('click', function() {
-    const fullName = fullNameInput.value.trim().toLowerCase();
-    let score = 0;
-    let visits = 0;
-    let press = false;
-    let swim = false;
-
-    if (fullName === 'иван иванов') {
-      score = 70;
-      visits = 25;
-      press = false;
-      swim = true;
-    } else if (fullName === 'петр петров') {
-      score = 40;
-      visits = 21;
-      press = true;
-      swim = false;
-    } else {
-      alert('Такого ученика нет');
-      return;
-    }
-
-    scoreElement.textContent = score;
-    visitsElement.textContent = visits;
-
-    if (Number(visits) < 23) {
-      visitsElement.parentElement.classList.add('red');
-    } else {
-      visitsElement.parentElement.classList.remove('red');
-    }
-
-    if (press) {
-      pressNormative.textContent = '✅';
-      pressNormative.classList.add('green');
-    } else {
-      pressNormative.textContent = '❌';
-      pressNormative.classList.remove('green');
-    }
-
-    if (swim) {
-      swimNormative.textContent = '✅';
-      swimNormative.classList.add('green');
-    } else {
-      swimNormative.textContent = '❌';
-      swimNormative.classList.remove('green');
-    }
-
-    normativesContainer.style.display = 'flex';
-    dataContainer.style.display = 'flex';
+  showDataButton.addEventListener('click', () => {
+    container.style.display = 'block';
   });
 });
