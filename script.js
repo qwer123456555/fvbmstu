c3ad3b8d4e4a54c23a11974a7d8da3443bbf52b6
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 18:30:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
-document.getElementById('showDataButton').addEventListener('click', function() {
-  var fullName = document.getElementById('fullNameInput').value.toLowerCase();
-
-  var score, visits, swimNormative, pressNormative;
-
-  if (fullName === 'иван иванов') {
-    score = 70;
-    visits = 25;
-    swimNormative = true;
-    pressNormative = true;
-  } else if (fullName === 'петр петров') {
-    score = 40;
-    visits = 21;
-    swimNormative = false;
-    pressNormative = true;
-  } else {
-    score = 0;
-    visits = 0;
-    swimNormative = false;
-    pressNormative = false;
-  }
-
-  document.getElementById('scoreValue').textContent = score;
-  document.getElementById('visitsValue').textContent = visits;
-
+window.addEventListener('DOMContentLoaded', function() {
+  var dataContainer = document.getElementById('dataContainer');
+  var normativesContainer = document.getElementById('normativesContainer');
   var scoreSquare = document.querySelector('.score');
   var visitsSquare = document.querySelector('.visits');
 
-  scoreSquare.classList.remove('green', 'red');
-  visitsSquare.classList.remove('green', 'red');
+  dataContainer.style.display = 'none';
+  normativesContainer.style.display = 'none';
 
-  if (score < 60) {
-    scoreSquare.classList.add('red');
-  } else {
-    scoreSquare.classList.add('green');
-  }
+  document.getElementById('showDataButton').addEventListener('click', function() {
+    var fullName = document.getElementById('fullNameInput').value.toLowerCase();
 
-  if (visits < 23) {
-    visitsSquare.classList.add('red');
-  } else {
-    visitsSquare.classList.add('green');
-  }
+    var score, visits, swimNormative, pressNormative;
 
-  var normativesContainer = document.getElementById('normativesContainer');
+    if (fullName === 'иван иванов') {
+      score = 70;
+      visits = 25;
+      swimNormative = true;
+      pressNormative = true;
+    } else if (fullName === 'петр петров') {
+      score = 40;
+      visits = 21;
+      swimNormative = false;
+      pressNormative = true;
+    } else {
+      score = 0;
+      visits = 0;
+      swimNormative = false;
+      pressNormative = false;
+    }
 
-  if (fullName === 'иван иванов' || fullName === 'петр петров') {
-    normativesContainer.style.display = 'block';
+    document.getElementById('scoreValue').textContent = score;
+    document.getElementById('visitsValue').textContent = visits;
 
-    var swimNormativeElement = document.getElementById('swimNormative');
-    var pressNormativeElement = document.getElementById('pressNormative');
+    scoreSquare.classList.remove('green', 'red');
+    visitsSquare.classList.remove('green', 'red');
 
-    swimNormativeElement.textContent = 'Норматив по плаванию: ' + (swimNormative ? '✅' : '❌');
-    pressNormativeElement.textContent = 'Норматив по прессу: ' + (pressNormative ? '✅' : '❌');
-  } else {
-    normativesContainer.style.display = 'none';
-  }
-  var dataContainer = document.getElementById('dataContainer');
-var normativesContainer = document.getElementById('normativesContainer');
+    if (score < 60) {
+      scoreSquare.classList.add('red');
+    } else {
+      scoreSquare.classList.add('green');
+    }
 
-dataContainer.style.display = 'none';
-normativesContainer.style.display = 'none';
+    if (visits < 23) {
+      visitsSquare.classList.add('red');
+    } else {
+      visitsSquare.classList.add('green');
+    }
 
-document.getElementById('showDataButton').addEventListener('click', function() {
-  // Остальной код обработчика кнопки здесь
-});
+    if (fullName === 'иван иванов' || fullName === 'петр петров') {
+      normativesContainer.style.display = 'block';
 
+      var swimNormativeElement = document.getElementById('swimNormative');
+      var pressNormativeElement = document.getElementById('pressNormative');
+
+      swimNormativeElement.textContent = 'Норматив по плаванию: ' + (swimNormative ? '✅' : '❌');
+      pressNormativeElement.textContent = 'Норматив по прессу: ' + (pressNormative ? '✅' : '❌');
+    } else {
+      normativesContainer.style.display = 'none';
+    }
+
+    dataContainer.style.display = 'block';
+  });
 });
