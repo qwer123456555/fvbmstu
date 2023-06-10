51b47f5c7c7e889befb915d6268a279d417064da
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 17:23:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,50 @@
-// Функция для отображения данных
-function showData() {
-  var fullName = document.getElementById("fullNameInput").value.toLowerCase();
-  var dataContainer = document.getElementById("dataContainer");
+document.getElementById('showDataButton').addEventListener('click', function() {
+  var fullName = document.getElementById('fullNameInput').value.toLowerCase();
 
-  // Очищаем содержимое контейнера данных
-  dataContainer.innerHTML = "";
+  var dataContainer = document.getElementById('dataContainer');
+  dataContainer.innerHTML = '';
 
-  // Создаем элементы для отображения данных ФИО
-  var fullNameHeading = document.createElement("h2");
-  fullNameHeading.textContent = "Ваше ФИО:";
-  var fullNameData = document.createElement("p");
-  fullNameData.textContent = fullName;
+  var normativesContainer = document.getElementById('normativesContainer');
+  normativesContainer.style.display = 'none';
 
-  // Добавляем элементы в контейнер данных
-  dataContainer.appendChild(fullNameHeading);
-  dataContainer.appendChild(fullNameData);
-
-  // Создаем квадраты для отображения баллов и посещений
-  var scoreSquare = document.createElement("div");
-  scoreSquare.classList.add("square");
-  var visitsSquare = document.createElement("div");
-  visitsSquare.classList.add("square");
-
-  // Устанавливаем значения баллов и посещений в зависимости от ФИО
-  var score, visits;
-  var normatives = {
-    "иван иванов": {
-      score: 70,
-      visits: 25,
-      swim: true,
-      press: false,
-    },
-    "петр петров": {
-      score: 40,
-      visits: 21,
-      swim: false,
-      press: true,
-    },
-  };
-
-  if (normatives.hasOwnProperty(fullName)) {
-    score = normatives[fullName].score;
-    visits = normatives[fullName].visits;
-    var swimNormative = normatives[fullName].swim ? "Галочка" : "Крестик";
-    var pressNormative = normatives[fullName].press ? "Галочка" : "Крестик";
-
-    scoreSquare.classList.add(score >= 60 ? "green" : "red");
-    visitsSquare.classList.add(visits >= 23 ? "green" : "red");
-
-    // Создаем элементы для отображения нормативов
-    var normativesHeading = document.createElement("h3");
-    normativesHeading.textContent = "Нормативы:";
-    var normativesList = document.createElement("ul");
-    var swimItem = document.createElement("li");
-    var pressItem = document.createElement("li");
-    swimItem.textContent = "Плавание: " + swimNormative;
-    pressItem.textContent = "Пресс: " + pressNormative;
-    normativesList.appendChild(swimItem);
-    normativesList.appendChild(pressItem);
-
-    // Добавляем элементы в контейнер данных
-    dataContainer.appendChild(scoreSquare);
-    dataContainer.appendChild(visitsSquare);
-    dataContainer.appendChild(normativesHeading);
-    dataContainer.appendChild(normativesList);
+  if (fullName === 'иван иванов') {
+    var score = 70;
+    var visits = 25;
+    var swimNormative = false;
+    var pressNormative = true;
+  } else if (fullName === 'петр петров') {
+    var score = 40;
+    var visits = 21;
+    var swimNormative = true;
+    var pressNormative = false;
   }
+
+  var scoreSquare = createSquare('Баллы', score, score < 60 ? 'red' : 'green');
+  var visitsSquare = createSquare('Посещения', visits, 'green');
+
+  dataContainer.appendChild(scoreSquare);
+  dataContainer.appendChild(visitsSquare);
+
+  if (fullName === 'иван иванов' || fullName === 'петр петров') {
+    normativesContainer.style.display = 'block';
+
+    var swimNormativeElement = document.getElementById('swimNormative');
+    var pressNormativeElement = document.getElementById('pressNormative');
+
+    swimNormativeElement.textContent = 'Норматив по плаванию: ' + (swimNormative ? 'Галочка' : 'Крестик');
+    pressNormativeElement.textContent = 'Норматив по прессу: ' + (pressNormative ? 'Галочка' : 'Крестик');
+  }
+});
+
+function createSquare(label, value, colorClass) {
+  var square = document.createElement('div');
+  square.className = 'square ' + colorClass;
+  square.textContent = value;
+
+  var labelElement = document.createElement('div');
+  labelElement.textContent = label;
+
+  square.appendChild(labelElement);
+
+  return square;
 }
-
-// Обработчик события для кнопки "Показать данные"
-var showDataButton = document.getElementById("showDataButton");
-showDataButton.addEventListener("click", showData);
-
-// Скрытие нормативов при загрузке страницы
-document.getElementById("normativesContainer").style.display = "none";
