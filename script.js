f0695ef66b1c18b55d65631161e7c2b08be2f65a
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 16:25:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function showData() {
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
+  var fullName = document.getElementById("fullNameInput").value;
+  var dataContainer = document.getElementById("dataContainer");
 
-    // Здесь можно добавить код для обработки ФИО и вывода данных
+  // Очищаем содержимое контейнера данных
+  dataContainer.innerHTML = "";
 
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerText = 'Данные для ' + name;
+  // Создаем элементы для отображения данных
+  var fullNameHeading = document.createElement("h2");
+  fullNameHeading.textContent = "Ваше ФИО:";
+  var fullNameData = document.createElement("p");
+  fullNameData.textContent = fullName;
+
+  // Добавляем элементы в контейнер данных
+  dataContainer.appendChild(fullNameHeading);
+  dataContainer.appendChild(fullNameData);
 }
