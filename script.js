dea16aabe4fcf7880c0fed173c53f05c7a8d33d9
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 19:24:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,45 +2,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const fullNameInput = document.getElementById('fullName');
   const startButton = document.getElementById('startButton');
   const container = document.querySelector('.container');
+  const studentNameElement = document.getElementById('studentName');
   const scoreElement = document.getElementById('score');
   const visitsElement = document.getElementById('visits');
   const swimNormativeElement = document.getElementById('swimNormative');
   const pressNormativeElement = document.getElementById('pressNormative');
-  const showDataButton = document.getElementById('showDataButton');
 
   startButton.addEventListener('click', () => {
-    const fullName = fullNameInput.value.toLowerCase();
+    const fullName = fullNameInput.value.toLowerCase().trim();
     const students = {
       'иван иванов': {
         score: 70,
         visits: 25,
         swimNormative: '✅',
-        pressNormative: '❌',
+        pressNormative: '❌'
       },
       'петр петров': {
         score: 40,
         visits: 21,
-        swimNormative: '❌',
-        pressNormative: '✅',
-      },
+        swimNormative: '✅',
+        pressNormative: '❌'
+      }
     };
 
     if (fullName in students) {
       const student = students[fullName];
+      studentNameElement.textContent = fullName;
       scoreElement.textContent = student.score;
       visitsElement.textContent = student.visits;
       swimNormativeElement.textContent = student.swimNormative;
       pressNormativeElement.textContent = student.pressNormative;
+
+      container.style.display = 'block';
+      document.body.style.overflow = 'hidden'; // Disable scrolling
     } else {
-      scoreElement.textContent = '';
-      visitsElement.textContent = '';
-      swimNormativeElement.textContent = '';
-      pressNormativeElement.textContent = '';
       alert('Такого ученика нет');
     }
-  });
-
-  showDataButton.addEventListener('click', () => {
-    container.style.display = 'block';
   });
 });
