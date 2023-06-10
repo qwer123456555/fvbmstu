2cb88a4614f2f869bf9bee0eb8f92c3e537b056a
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 13:42:58 +0300

    Add files via upload

---
+++ EmptyJSFile.js
@@ -0,0 +1,9 @@
+﻿document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Предотвращаем отправку формы
+
+    var fullName = document.getElementById("fullName").value;
+    var result = document.getElementById("result");
+
+    // Выводим данные
+    result.innerHTML = "Ваши данные: " + fullName;
+});
