29f5366dfe7eab134c42bea3de0a0b05af910323
qwer123456555 <135661745+qwer123456555@users.noreply.github.com>
2023-06-10 16:29:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ function showData() {
   // Добавляем элементы в контейнер данных
   dataContainer.appendChild(fullNameHeading);
   dataContainer.appendChild(fullNameData);
+
+  // Обработка определенных ФИО и вывод соответствующих данных
+  if (fullName === "Иван Иванов") {
+    var data1 = document.createElement("p");
+    data1.classList.add("data");
+    data1.textContent = "Данные для Ивана Иванова";
+    dataContainer.appendChild(data1);
+  } else if (fullName === "Петр Петров") {
+    var data2 = document.createElement("p");
+    data2.classList.add("data");
+    data2.textContent = "Данные для Петра Петрова";
+    dataContainer.appendChild(data2);
+  } else {
+    var dataDefault = document.createElement("p");
+    dataDefault.classList.add("data");
+    dataDefault.textContent = "Данные для другого ФИО";
+    dataContainer.appendChild(dataDefault);
+  }
 }
